a22ee34565e864116bb7ab735ccd17c367937341
ricozhuthegreat <ricozhuthegreat@hotmail.com>
2020-05-17 10:46:28 -0400

    Bug Patch

--- src/js/main.js
+++ src/js/main.js
@@ -4,11 +4,15 @@ document.getElementById("mix").onclick  = () => {
 
   xhttp.onreadystatechange = function() {
       if (this.readyState == 4 && this.status == 200) {
+        console.log("SUCCESS");
           var xhttpJSON = JSON.parse(this.responseText);
           console.log(xhttpJSON);
       }
   };
 
-  xhttp.open("POST", "https://console.echoAR.xyz/post?key=blue-dawn-7175&entry=1d4b3709-99c3-4c97-b96a-fbfa9b95df22&data=scale&value=50", true);
+  xhttp.open("POST", "https://console.echoAR.xyz/post?key=blue-dawn-7175&entry=1d4b3709-99c3-4c97-b96a-fbfa9b95df22&data=yAngle&value=10", true);
+  xhttp.send();
+
+  console.log("MIXING");
 
 }
