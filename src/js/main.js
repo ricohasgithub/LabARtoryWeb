0e9064e248ebe13b35ca33e2d7c79a1b8e30b79d
ricozhuthegreat <ricozhuthegreat@hotmail.com>
2020-05-17 10:35:51 -0400

    Added JS for Animations

--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,14 @@
+document.getElementById("mix").onclick  = () => {
 
-// https://console.echoAR.xyz/post?key=<API_KEY>&data=<DATA>&value=<VALUE>
+  var xhttp = new XMLHttpRequest();
+
+  xhttp.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+          var xhttpJSON = JSON.parse(this.responseText);
+          console.log(xhttpJSON);
+      }
+  };
+
+  xhttp.open("POST", "https://console.echoAR.xyz/post?key=blue-dawn-7175&data=yAngledirection&value=10" + , true);
+
+}
